d7bb0dc264d61a686176a335ce6cd7ea4e3b857e
Badar-Ur-Zaman <hbrsadiqian123@gmail.com>
2026-04-05 23:32:39 +0500

    Added 1024-dim embedding model from hugging face

--- app/services/resources.py
+++ app/services/resources.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from pinecone import Pinecone
 from transformers import AutoTokenizer, AutoModel
-from sentence_transformers import SentenceTransformer
+
 from huggingface_hub import InferenceClient
 
 load_dotenv()
@@ -12,7 +12,7 @@ class Resources:
     def __init__(self):
 
         # HuggingFace embedding model
-        MODEL_NAME = "google/embeddinggemma-300m"
+        MODEL_NAME = "BAAI/bge-large-en-v1.5"
 
         self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, token=os.getenv("HF_TOKEN"))
         self.embedding_model = AutoModel.from_pretrained(MODEL_NAME)
@@ -24,7 +24,13 @@ class Resources:
 
         # Pinecone
         self.pinecone = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-        self.index = self.pinecone.Index("bank-customer-support")
+        self.index = self.pinecone.Index("badar-index")
 
 
 resources = None
+
+def get_resources() -> Resources:
+    global resources
+    if resources is None:
+        resources = Resources()
+    return resources
